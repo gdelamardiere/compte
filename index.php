6c4d6a6e31a4c8755ec2552db9b79ef9518d7299
guerric <gdelamardiere@gmail.com>
2013-03-12 15:43:15 +0100

    ajout init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="utf-8">
+    <title>Base Admin - Live Preview - WrapBootstrap</title>
+    <meta name="description" content="Live preview for Base Admin at WrapBootstrap">
+    <meta name="viewport" content="width=1024">
+    <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" type="text/css" media="screen" href="//s3.amazonaws.com/wrapbootstrap/live/css/screen_preview.css">
+    <script src="//s3.amazonaws.com/wrapbootstrap/live/js/behavior.js"></script>
+    <link rel="shortcut icon" href="//s3.amazonaws.com/wrapbootstrap/live/imgs/favicon.ico">
+</head>
+<body>
+    <div id="header"></div>
+    <div id="main">
+        <div class="container">
+        </div>
+        
+    </div>
+    <div id="body"></div>
+</body>
+</html>
